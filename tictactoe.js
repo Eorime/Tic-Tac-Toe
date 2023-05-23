dc62c11ef4445770bd9255b9e4b9373d02afe5e0
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2023-05-23 17:29:10 +0400

    Added a variable to save the index of the current box.

--- tictactoe.js
+++ tictactoe.js
@@ -9,32 +9,51 @@ const WIN_COMBINATIONS = [
   [2, 4, 6],
 ];
 
+let boarded = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// roca daechireba, shesabamis wevrs 1,2 dasvam
+let currentPlayer = 1;
+
+// get every element and compare the choices of the player
+// store the x choices
+
 //Variables for the O and X pseudo elements
 const THE_CIRCLE = "o";
 const THE_X = "x";
 const allBoxes = document.querySelectorAll("[data-box]");
 let circleTurn;
 
+// Runs through the entire array and applies the event listener to every single box
 allBoxes.forEach((box) => {
   box.addEventListener("click", clicked, { once: true });
 });
-
-function switchPlayer() {}
 
 function clicked(event) {
   let currentBox = event.target;
   const currentType = circleTurn ? THE_CIRCLE : THE_X;
   placeMark(currentBox, currentType);
   switchPlayer();
-  //check win
-  //check draw
+  let currentBoxIndex = Number(currentBox.classList[1]) - 1;
+  console.log(currentBoxIndex);
+  //   checkWin();
+  //   checkDraw(); toca yvela sheivseba
 
   //Places the mark in the box based on the current type
   function placeMark() {
     currentBox.classList.add(currentType);
   }
-  //This function switches a player
+
+  //This function switches the player
   function switchPlayer() {
     circleTurn = !circleTurn;
   }
 }
+
+//Checks for the win
+function checkWin() {
+  //ro daechira amoagde
+  // tu emtxveva romelime romelime winning combos vgdebt congrats
+  // mere vnaxavt circle a tu x
+}
+
+//Checks for the draw
